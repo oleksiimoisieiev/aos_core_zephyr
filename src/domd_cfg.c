dea350ad84b4cccf612c80b2d8b0b56962b4be68
Sergiy Kibrik <Sergiy_Kibrik@epam.com>
2023-03-21 11:46:30 +0200

    adjust domd_cfg to match aos demo

--- src/domd_cfg.c
+++ src/domd_cfg.c
@@ -9,370 +9,133 @@
 #include <zephyr/xen/public/domctl.h>
 
 static char *domd_dtdevs[] = {
-	    "/soc/dma-controller@e6460000",
-	    "/soc/dma-controller@e6470000",
-	    "/soc/dma-controller@e6700000",
-	    "/soc/dma-controller@e7300000",
-	    "/soc/dma-controller@e7310000",
-	    "/soc/dma-controller@ec700000",
-	    "/soc/dma-controller@ec720000",
-	    "/soc/ethernet@e6800000",
-	    "/soc/video@e6ef0000",
-	    "/soc/video@e6ef1000",
-	    "/soc/video@e6ef2000",
-	    "/soc/video@e6ef3000",
-	    "/soc/video@e6ef4000",
-	    "/soc/video@e6ef5000",
-	    "/soc/video@e6ef6000",
-	    "/soc/video@e6ef7000",
-	    "/soc/usb@ee000000",
-	    "/soc/dma-controller@e65a0000",
-	    "/soc/dma-controller@e65b0000",
-	    "/soc/mmc@ee100000",
-	    "/soc/mmc@ee140000",
-	    "/soc/mmc@ee160000",
-	    "/soc/usb@ee080100",
-	    "/soc/usb@ee020000",
-	    "/soc/usb@ee0a0100",
-	    "/soc/usb@ee0c0100",
-	    "/soc/usb@ee080000",
-	    "/soc/usb@ee0a0000",
-	    "/soc/usb@ee0c0000",
-	    "/soc/imr-lx4@fe860000",
-	    "/soc/imr-lx4@fe870000",
-	    "/soc/imr-lx4@fe880000",
-	    "/soc/imr-lx4@fe890000",
-	    "/soc/vspm@fe920000",
-	    "/soc/vspm@fe960000",
-	    "/soc/vspm@fe9a0000",
-	    "/soc/vspm@fe9b0000",
-	    "/soc/fcp@fea27000",
-	    "/soc/fcp@fea2f000",
-	    "/soc/fcp@fea37000",
-	    "/soc/fdpm@fe940000",
-	    "/soc/fdpm@fe944000",
-	    "/soc/hdmi@fead0000",
-	    "/soc/hdmi@feae0000",
-	    "/soc/gsx_pv0_domd",
-	    "/soc/gsx_pv1_domd",
-	    "/soc/gsx_pv2_domd",
-	    "/soc/gsx_pv3_domd",
-	    "/soc/fcpcs_vc0",
-	    "/soc/fcpcs_vc1",
-	    "/soc/pcie@fe000000",
-	    "/soc/pcie@ee800000",
-	    "/soc/impdm0",
-	    "/soc/dma-controller@ffc10000",
-	    "/soc/dma-controller@ffc20000",
+	"/soc/dma-controller@e7350000",
+	"/soc/dma-controller@e7351000",
+	"/soc/mmc@ee140000",
+	"/soc/ethernet@e68c0000",
+	"/soc/pcie@e65d0000",
+};
+static char *dt_passthrough_nodes[] = {
+	"/extal",
+	"/extalr",
+	"/pcie_bus",
+	"/scif",
+	"/soc",
+	"/regulator-1p8v",
+	"/regulator-3p3v",
+	"/reserved-memory",
 };
 
 static struct xen_domain_iomem domd_iomems[] = {
-
-	/* #re-mapped MM_LOSSY_SHARED_MEM_ADDR page */
-	{ .first_gfn = 0xe0000, .first_mfn = 0x47fd7, .nr_mfns = 0x1 },
-	{ .first_mfn = 0xe6260, .nr_mfns = 0x1},
 	{ .first_mfn = 0xe6020, .nr_mfns = 0x1},
 	{ .first_mfn = 0xe6050, .nr_mfns = 0x1},
 	{ .first_mfn = 0xe6051, .nr_mfns = 0x1},
-	{ .first_mfn = 0xe6052, .nr_mfns = 0x1},	/* GPIO2 */
-	{ .first_mfn = 0xe6053, .nr_mfns = 0x1},
-	{ .first_mfn = 0xe6054, .nr_mfns = 0x1},
-	{ .first_mfn = 0xe6055, .nr_mfns = 0x1},
-	{ .first_mfn = 0xe6060, .nr_mfns = 0x1},	/* PFC */
-	{ .first_mfn = 0xe60a0, .nr_mfns = 0x1},
-	{ .first_mfn = 0xe6150, .nr_mfns = 0x1},	/* CPG */
-	{ .first_mfn = 0xe6160, .nr_mfns = 0x1},	/* RST */
-	{ .first_mfn = 0xe6180, .nr_mfns = 0x1},	/* SYSC */
+	{ .first_mfn = 0xe6150, .nr_mfns = 0x4},	/* CPG */
+	{ .first_mfn = 0xe6160, .nr_mfns = 0x4},	/* RST */
+	{ .first_mfn = 0xe6180, .nr_mfns = 0x4},	/* SYSC */
+	{ .first_mfn = 0xe6500, .nr_mfns = 0x1},
+	{ .first_mfn = 0xe66d8, .nr_mfns = 0x1},
+	{ .first_mfn = 0xe68c0, .nr_mfns = 0x20},	/* EtherAVB */
+	{ .first_mfn = 0xe6444, .nr_mfns = 0x3},	/* EtherAVB */
+	{ .first_mfn = 0xe6550, .nr_mfns = 0x1},
+	{ .first_mfn = 0xe6e60, .nr_mfns = 0x1},
+	{ .first_mfn = 0xe7350, .nr_mfns = 0x1},
+	{ .first_mfn = 0xe7300, .nr_mfns = 0x10},
+	{ .first_mfn = 0xe7351, .nr_mfns = 0x1},
+	{ .first_mfn = 0xe7310, .nr_mfns = 0x10},
+	{ .first_mfn = 0xee140, .nr_mfns = 0x2},
+	{ .first_mfn = 0xfff00, .nr_mfns = 0x1},
+	{ .first_mfn = 0xffd60, .nr_mfns = 0x1},
+	{ .first_mfn = 0xffc10, .nr_mfns = 0x10},
+	{ .first_mfn = 0xffd61, .nr_mfns = 0x1},
+	{ .first_mfn = 0xffc20, .nr_mfns = 0x10},
+	{ .first_mfn = 0xffd62, .nr_mfns = 0x1},
+	{ .first_mfn = 0xffd70, .nr_mfns = 0x10},
+	{ .first_mfn = 0xffd63, .nr_mfns = 0x1},
+	{ .first_mfn = 0xffd80, .nr_mfns = 0x10},
+	{ .first_mfn = 0xe65d3, .nr_mfns = 0x2},
+	{ .first_mfn = 0xe65d5, .nr_mfns = 0x2},
+	{ .first_mfn = 0xe65d6, .nr_mfns = 0x1},
+	{ .first_mfn = 0xe65d7, .nr_mfns = 0x1},
 	{ .first_mfn = 0xe6198, .nr_mfns = 0x1},
 	{ .first_mfn = 0xe61a0, .nr_mfns = 0x1},
-	{ .first_mfn = 0xe61a8, .nr_mfns = 0x1},
-	{ .first_mfn = 0xe61c0, .nr_mfns = 0x1},
-	{ .first_mfn = 0xe6510, .nr_mfns = 0x1},
-	{ .first_mfn = 0xe66d8, .nr_mfns = 0x1},
-	{ .first_mfn = 0xe60b0, .nr_mfns = 0x1},
-	{ .first_mfn = 0xe6550, .nr_mfns = 0x1},
-	{ .first_mfn = 0xe6590, .nr_mfns = 0x1},
-	{ .first_mfn = 0xe65a0, .nr_mfns = 0x1},
-	{ .first_mfn = 0xe65b0, .nr_mfns = 0x1},
-	{ .first_mfn = 0xe6460, .nr_mfns = 0x1},
-	{ .first_mfn = 0xe6470, .nr_mfns = 0x1},
-	{ .first_mfn = 0xe65ee, .nr_mfns = 0x1},
-	{ .first_mfn = 0xe6601, .nr_mfns = 0x1},
-	{ .first_mfn = 0xe6700, .nr_mfns = 0x10},
-	{ .first_mfn = 0xe7300, .nr_mfns = 0x10},
-	{ .first_mfn = 0xe7310, .nr_mfns = 0x10},
-	{ .first_mfn = 0xe67e0, .nr_mfns = 0x11},
-	{ .first_mfn = 0xe6800, .nr_mfns = 0x1},	/* EtherAVB */
-	{ .first_mfn = 0xe6a00, .nr_mfns = 0x10},	/* EtherAVB */
-	{ .first_mfn = 0xe6e31, .nr_mfns = 0x1},
-	{ .first_mfn = 0xe6e32, .nr_mfns = 0x1},
-	{ .first_mfn = 0xe6ef0, .nr_mfns = 0x1},
-	{ .first_mfn = 0xe6ef1, .nr_mfns = 0x1},
-	{ .first_mfn = 0xe6ef2, .nr_mfns = 0x1},
-	{ .first_mfn = 0xe6ef3, .nr_mfns = 0x1},
-	{ .first_mfn = 0xe6ef4, .nr_mfns = 0x1},
-	{ .first_mfn = 0xe6ef5, .nr_mfns = 0x1},
-	{ .first_mfn = 0xe6ef6, .nr_mfns = 0x1},
-	{ .first_mfn = 0xe6ef7, .nr_mfns = 0x1},
-	{ .first_mfn = 0xec500, .nr_mfns = 0x1},
-	{ .first_mfn = 0xec5a0, .nr_mfns = 0x1},
-	{ .first_mfn = 0xec540, .nr_mfns = 0x1},
-	{ .first_mfn = 0xec541, .nr_mfns = 0x1},
-	{ .first_mfn = 0xec760, .nr_mfns = 0x1},
-	{ .first_mfn = 0xec000, .nr_mfns = 0x1},
-	{ .first_mfn = 0xec008, .nr_mfns = 0x1},
-	{ .first_mfn = 0xec700, .nr_mfns = 0x10},
-	{ .first_mfn = 0xec720, .nr_mfns = 0x10},
-	{ .first_mfn = 0xee000, .nr_mfns = 0x1},
-	{ .first_mfn = 0xee020, .nr_mfns = 0x1},
-	{ .first_mfn = 0xee080, .nr_mfns = 0x1},
-	{ .first_mfn = 0xee0a0, .nr_mfns = 0x1},
-	{ .first_mfn = 0xee0c0, .nr_mfns = 0x1},
-	{ .first_mfn = 0xee100, .nr_mfns = 0x2},
-	{ .first_mfn = 0xee140, .nr_mfns = 0x2},
-	{ .first_mfn = 0xee160, .nr_mfns = 0x2},
-	{ .first_mfn = 0xfd000, .nr_mfns = 0x40},
-	{ .first_mfn = 0xfe000, .nr_mfns = 0x80},
-	{ .first_mfn = 0xee800, .nr_mfns = 0x80},
-	{ .first_mfn = 0xfe860, .nr_mfns = 0x2},
-	{ .first_mfn = 0xfe870, .nr_mfns = 0x2},
-	{ .first_mfn = 0xfe880, .nr_mfns = 0x2},
-	{ .first_mfn = 0xfe890, .nr_mfns = 0x2},
-	{ .first_mfn = 0xfe90f, .nr_mfns = 0x1},
-	{ .first_mfn = 0xfe8d0, .nr_mfns = 0x1},
-	{ .first_mfn = 0xfe910, .nr_mfns = 0x1},
-	{ .first_mfn = 0xfe900, .nr_mfns = 0x1},
-	{ .first_mfn = 0xfe940, .nr_mfns = 0x3},
-	{ .first_mfn = 0xfe950, .nr_mfns = 0x1},
-	{ .first_mfn = 0xfe944, .nr_mfns = 0x3},
-	{ .first_mfn = 0xfe951, .nr_mfns = 0x1},
-	{ .first_mfn = 0xfea27, .nr_mfns = 0x1},
-	{ .first_mfn = 0xfea2f, .nr_mfns = 0x1},
-	{ .first_mfn = 0xfea37, .nr_mfns = 0x1},
-	{ .first_mfn = 0xfe960, .nr_mfns = 0x8},
-	{ .first_mfn = 0xfe96f, .nr_mfns = 0x1},
-	{ .first_mfn = 0xfe920, .nr_mfns = 0x8},
-	{ .first_mfn = 0xfe92f, .nr_mfns = 0x1},
-	{ .first_mfn = 0xfea20, .nr_mfns = 0x5},
-	{ .first_mfn = 0xfea28, .nr_mfns = 0x5},
-	{ .first_mfn = 0xfea30, .nr_mfns = 0x5},
-	{ .first_mfn = 0xfe9a0, .nr_mfns = 0x8},
-	{ .first_mfn = 0xfe9af, .nr_mfns = 0x1},
-	{ .first_mfn = 0xfe9b0, .nr_mfns = 0x8},
-	{ .first_mfn = 0xfe9bf, .nr_mfns = 0x1},
-	{ .first_mfn = 0xfea80, .nr_mfns = 0x10},
-	{ .first_mfn = 0xfeaa0, .nr_mfns = 0x10},
-	{ .first_mfn = 0xfead0, .nr_mfns = 0x10},
-	{ .first_mfn = 0xfeae0, .nr_mfns = 0x10},
-	{ .first_mfn = 0xfeb00, .nr_mfns = 0x80},
-	{ .first_mfn = 0xfeb90, .nr_mfns = 0x1},
-	{ .first_mfn = 0xfff00, .nr_mfns = 0x1},
-	{ .first_mfn = 0xffa00, .nr_mfns = 0x4},
-	{ .first_mfn = 0xff900, .nr_mfns = 0x20},
-	{ .first_mfn = 0xff920, .nr_mfns = 0x20},
-	{ .first_mfn = 0xff940, .nr_mfns = 0x20},
-	{ .first_mfn = 0xff960, .nr_mfns = 0x20},
-	{ .first_mfn = 0xff980, .nr_mfns = 0x10},
-	{ .first_mfn = 0xff990, .nr_mfns = 0x10},
-	{ .first_mfn = 0xff9c0, .nr_mfns = 0x10},
-	{ .first_mfn = 0xffa10, .nr_mfns = 0x4},
-	{ .first_mfn = 0xffa40, .nr_mfns = 0x20},
-	{ .first_mfn = 0xff8d0, .nr_mfns = 0x1},
-	{ .first_mfn = 0x54000, .nr_mfns = 0x3000},
-	{ .first_mfn = 0xffc10, .nr_mfns = 0x10},
-	{ .first_mfn = 0xffc20, .nr_mfns = 0x10},
-	{ .first_mfn = 0xfea40, .nr_mfns = 0x1},
-	{ .first_mfn = 0xfea50, .nr_mfns = 0x1},
-	{ .first_mfn = 0xfea60, .nr_mfns = 0x1},
-	{ .first_mfn = 0xfea70, .nr_mfns = 0x1},
-	{ .first_mfn = 0xe6e68, .nr_mfns = 0x1},	/* SCIF1 */
+	{ .first_mfn = 0xe61a0, .nr_mfns = 0x1},
+	{ .first_mfn = 0xe61b0, .nr_mfns = 0x1},
+	{ .first_mfn = 0xe6260, .nr_mfns = 0x10},
+	{ .first_mfn = 0xdfd91, .nr_mfns = 0x1},
+//	{ .first_gfn = 0x47fc7, .first_mfn = 0x37fc7, .nr_mfns = 0x2},
 };
 
 static uint32_t domd_irqs[] = {
-		// gpio@e6050000
-		    36,
-		// gpio@e6051000
-		    37,
-		// gpio@e6052000
-		    38,
-		// gpio@e6053000
-		    39,
-		// gpio@e6054000
-		    40,
-		// gpio@e6055000
-		    41,
-		// gpio@e6055400
-		    42,
-		// gpio@e6055800
-		    43,
-		// thermal@e6198000
-		    99, 100, 101,
-		// interrupt-controller@e61c0000
-		    32, 33, 34, 35, 193, 50,
-		// i2c@e6510000
-		    318,
-		// i2c@e66d8000
-		    51,
-		// i2c@e60b0000
-		    205,
-		// serial@e6550000
-		    187,
-		// usb@e6590000
-		    139,
-		// dma-controller@e65a0000
-		    141,
-		// dma-controller@e65b0000
-		    142,
-		// dma-controller@e6460000
-		    66,
-		// dma-controller@e6470000
-		    67,
-		// crypto@e6601000
-		    103,
-		// dma-controller@e6700000
-		    231, 232, 233, 234, 235, 236, 237, 238, 239, 240, 241, 242, 243, 244, 245, 246, 247,
-		// dma-controller@e7300000
-		    251, 345, 252, 346, 248, 340, 341, 342, 343, 344, 249, 250, 347, 348, 349, 350, 351,
-		// dma-controller@e7310000
-		    448, 449, 450, 451, 452, 453, 454, 455, 456, 457, 458, 459, 460, 461, 462, 463, 429,
-		// ethernet@e6800000
-		    71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95,
-		// video@e6ef0000
-		    220,
-		// video@e6ef1000
-		    221,
-		// video@e6ef2000
-		    222,
-		// video@e6ef3000
-		    223,
-		// video@e6ef4000
-		    206,
-		// video@e6ef5000
-		    207,
-		// video@e6ef6000
-		    208,
-		// video@e6ef7000
-		    203,
-		// src-0
-		    384,
-		// src-1
-		    385,
-		// src-2
-		    386,
-		// src-3
-		    387,
-		// src-4
-		    388,
-		// src-5
-		    389,
-		// src-6
-		    390,
-		// src-7
-		    391,
-		// src-8
-		    392,
-		// src-9
-		    393,
-		// ssi-0
-		    402,
-		// ssi-1
-		    403,
-		// ssi-2
-		    404,
-		// ssi-3
-		    405,
-		// ssi-4
-		    406,
-		// ssi-5
-		    407,
-		// ssi-6
-		    408,
-		// ssi-7
-		    409,
-		// ssi-8
-		    410,
-		// ssi-9
-		    411,
-		// dma-controller@ec700000
-		    352, 353, 354, 355, 356, 357, 358, 359, 360, 361, 362, 363, 364, 365, 366, 367, 382,
-		// dma-controller@ec720000
-		    383, 368, 369, 370, 371, 372, 373, 374, 375, 376, 377, 378, 379, 380, 381, 414, 415,
-		// adsp@ec800000
-		//   269,
-		// usb@ee000000
-		    134,
-		// usb@ee020000
-		    136,
-		// usb@ee080000
-		    140,
-		// usb@ee0a0000
-		    144,
-		// usb@ee0c0000
-		    145,
-		// mmc@ee100000
-		    197,
-		// mmc@ee140000
-		    199,
-		// mmc@ee160000
-		    200,
-		// gsx@fd000000
-		    151,
-		// pcie@fe000000
-		    148, 149, 150,
-		// pcie@ee800000
-		    180, 181, 182,
-		// imr-lx4@fe860000
-		    224,
-		// imr-lx4@fe870000
-		    225,
-		// imr-lx4@fe880000
-		    226,
-		// imr-lx4@fe890000
-		    227,
-		// vcp4@fe8d0000
-		    412, 413, 255,
-		// vcp4@fe910000
-		    292, 293,
-		// vcp4@fe900000
-		    272, 273,
-		// fdpm@fe940000
-		    294,
-		// fdpm@fe944000
-		    295,
-		// vspm@fe960000
-		    298,
-		// vspm@fe920000
-		    497,
-		// vsp@fea20000
-		    498,
-		// vsp@fea28000
-		    499,
-		// vsp@fea30000
-		    500,
-		// vspm@fe9a0000
-		    476,
-		// vspm@fe9b0000
-		    477,
-		// csi2@fea80000
-		    216,
-		// csi2@feaa0000
-		    278,
-		// hdmi@fead0000
-		    421,
-		// hdmi@feae0000
-		    468,
-		// display@feb00000
-		    288, 300, 301, 302,
-		// impdes0
-		    471,
-		// imprtt
-		    425,
-		// dma-controller@ffc10000
-		    480, 481, 482, 483, 484, 485, 486, 487, 488,
-		// dma-controller@ffc20000
-		    501, 489, 490, 491, 492, 493, 494, 495, 496,
-		/* serial@e6e68000 */
-			185,
+	//gpio@e6050180
+	854,
+	//gpio@e6050980
+	855,
+	//gpio@e6051180
+	856,
+	//gpio@e6051980
+	857,
+	//i2c@e6500000
+	270,
+	//i2c@e66d8000
+	274,
+	//serial@e6550000
+	278,
+	//serial@e6e60000
+	281,
+	//serial@e6c50000 - used by Xen
+	//   284,
+	//dma-controller@e7350000
+	133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 149,
+	//dma-controller@e7351000
+	151, 158, 157, 152, 179, 180, 181, 182, 183, 184, 185, 186, 155, 156, 154, 153, 159,
+	//mmc@ee140000
+	268,
+	//dma-controller@ffd60000
+	64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 61, 62, 63,
+	//dma-controller@ffd61000
+	79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95,
+	//dma-controller@ffd62000
+	97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113,
+	//dma-controller@ffd63000
+	128, 129, 130, 131, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127,
+	//pcie@e65d0000
+	448, 449, 450, 451, 452, 453, 454,
+	//mfis@e6260000
+	352,
+	//iccom01
+	354,
+	//iccom02
+	356,
+	//iccom03
+	358,
+	//iccom04
+	360,
+	//iccom05
+	362,
+	//iccom06
+	364,
+	//iccom07
+	366,
+	//iccom016
+	386,
+	//iccom017
+	388,
+	//iccom018
+	390,
+	//iccom019
+	392,
+	//iccom020
+	394,
+	//iccom021
+	396,
+	//iccom022
+	398,
+	//iccom023
+	400,
+	//ethernet@e68c0000
+	312, 313, 314, 315, 316, 317, 318, 319, 320, 321,
+	322, 323, 324, 325, 326, 327, 328, 329, 330, 331,
 };
 
 extern char __img_ipl_start[];
@@ -398,24 +161,26 @@ static ssize_t get_ipl_image_size(void *image_info, uint64_t *size)
 struct xen_domain_cfg domd_cfg = {
 	.mem_kb = 0x100000, /* 1Gb */
 
-	.flags = (XEN_DOMCTL_CDF_hvm | XEN_DOMCTL_CDF_hap),
+	.flags = (XEN_DOMCTL_CDF_hvm | XEN_DOMCTL_CDF_hap | XEN_DOMCTL_CDF_iommu),
 	.max_evtchns = 10,
 	.max_vcpus = 4,
 	.gnt_frames = 32,
 	.max_maptrack_frames = 1,
 
 	.iomems = domd_iomems,
-	.nr_iomems = 0,
+	.nr_iomems = ARRAY_SIZE(domd_iomems),
 
 	.irqs = domd_irqs,
-	.nr_irqs = 0,
+	.nr_irqs = ARRAY_SIZE(domd_irqs),
 
 	.gic_version = XEN_DOMCTL_CONFIG_GIC_V3,
 	.tee_type = XEN_DOMCTL_CONFIG_TEE_NONE,
 
 	.dtdevs = domd_dtdevs,
-	.nr_dtdevs = 0,
+	.nr_dtdevs = ARRAY_SIZE(domd_dtdevs),
 
+	.dt_passthrough = dt_passthrough_nodes,
+	.nr_dt_passthrough = ARRAY_SIZE(dt_passthrough_nodes),
 	.load_image_bytes = load_ipl_image,
 	.get_image_size = get_ipl_image_size,
 	.image_info = NULL,
