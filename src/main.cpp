746a2e40bf172135202ab3073c5af98319d49d99
Oleksii Moisieiev <oleksii_moisieiev@epam.com>
2023-03-21 16:51:45 +0200

    b
    Signed-off-by: Oleksii Moisieiev <oleksii_moisieiev@epam.com>

--- src/main.cpp
+++ src/main.cpp
@@ -17,10 +17,101 @@
 
 #if defined(DEBUG)
 extern "C" {
+#include <zephyr/storage/flash_map.h>
 
 #include <storage.h>
 #include <xen_dom_mgmt.h>
 #include <xrun.h>
+
+#define PARTITION_NODE \
+	DT_NODELABEL(storage)
+
+static int littlefs_flash_erase(unsigned int id)
+{
+	const struct flash_area *pfa;
+	int rc;
+
+	rc = flash_area_open(id, &pfa);
+	if (rc < 0) {
+		printk("FAIL: unable to find flash area %u: %d\n",
+			id, rc);
+		return rc;
+	}
+
+	printk("Area %u at 0x%x on %s for %u bytes\n",
+		   id, (unsigned int)pfa->fa_off, pfa->fa_dev->name,
+		   (unsigned int)pfa->fa_size);
+
+	flash_area_close(pfa);
+	return rc;
+}
+
+#if DT_NODE_EXISTS(PARTITION_NODE)
+FS_FSTAB_DECLARE_ENTRY(PARTITION_NODE);
+#else /* PARTITION_NODE */
+FS_LITTLEFS_DECLARE_DEFAULT_CONFIG(storage);
+static struct fs_mount_t lfs_storage_mnt = {
+	.type = FS_LITTLEFS,
+	.fs_data = &storage,
+	.storage_dev = (void *)FIXED_PARTITION_ID(storage_partition),
+	.mnt_point = "/lfs",
+};
+#endif /* PARTITION_NODE */
+
+	struct fs_mount_t *mp =
+#if DT_NODE_EXISTS(PARTITION_NODE)
+		&FS_FSTAB_ENTRY(PARTITION_NODE)
+#else
+		&lfs_storage_mnt
+#endif
+		;
+
+static int littlefs_mount(struct fs_mount_t *mp)
+{
+	int rc;
+
+	rc = littlefs_flash_erase((uintptr_t)mp->storage_dev);
+	if (rc < 0) {
+		return rc;
+	}
+
+	/* Do not mount if auto-mount has been enabled */
+#if !DT_NODE_EXISTS(PARTITION_NODE) || \
+	!(FSTAB_ENTRY_DT_MOUNT_FLAGS(PARTITION_NODE) & FS_MOUNT_FLAG_AUTOMOUNT)
+	rc = fs_mount(mp);
+	if (rc < 0) {
+		printk("Mount id %" PRIuPTR " at %s: %d\n",
+			(uintptr_t)mp->storage_dev, mp->mnt_point, rc);
+		return rc;
+	}
+	printk("%s mount: %d\n", mp->mnt_point, rc);
+#else
+	printk("%s automounted\n", mp->mnt_point);
+#endif
+
+	return 0;
+}
+
+static int littlefs_umount(struct fs_mount_t *mp)
+{
+	int rc;
+
+	/* Do not mount if auto-mount has been enabled */
+#if !DT_NODE_EXISTS(PARTITION_NODE) ||									\
+    !(FSTAB_ENTRY_DT_MOUNT_FLAGS(PARTITION_NODE) & FS_MOUNT_FLAG_AUTOMOUNT)
+	rc = fs_umount(mp);
+	if (rc < 0) {
+		printk("Fail to umount id %" PRIuPTR " at %s: %d\n", (uintptr_t)mp->storage_dev,
+				mp->mnt_point, rc);
+		return rc;
+	}
+	printk("%s unmounted: %d\n", mp->mnt_point, rc);
+#else
+	printk("%s automounted, ignore\n", mp->mnt_point);
+#endif
+
+	return 0;
+}
 
 char json_msg[] = "{"
 	 "\"ociVersion\" : \"1.0.1\", "
@@ -52,6 +143,12 @@ static void prepare_configs()
 
 	init_root();
 
+	ret = littlefs_mount(mp);
+	if (ret) {
+		printk("Unable to mount: %d\n", ret);
+		return;
+	}
+
 	printk("dtb = %p\n", (void *)__dtb_unikraft_start);
 	ret = write_file("/lfs", "config.json", json_msg, sizeof(json_msg));
 	printk("Write config.json file ret = %d\n", ret);
@@ -63,6 +160,10 @@ static void prepare_configs()
 	printk("Write unikraft.dtb file ret = %d\n", ret);
 	ret = lsdir("/lfs");
 	printk("lsdir result = %d\n", ret);
+
+	ret = littlefs_umount(mp);
+	if (ret)
+		printk("Error, unable to unmount %d\n", ret);
 }
 
 } /* extern "C" */
